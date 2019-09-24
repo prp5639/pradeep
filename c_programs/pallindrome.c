6989169032d7c78af193354ae366fefde3c567a9
pradi <prp5639@gmail.com>
2019-09-24 15:49:00 +0000

    directory added

---
+++ c_programs/pallindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+pal()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+    printf("\n\nEnter an integer: ");
+    scanf("%d", &n);
+    originalInteger = n;
+    // reversed integer is stored in variable 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
+    // palindrome if orignalInteger and reversedInteger are equal
+    if (originalInteger == reversedInteger)
+        printf("%d is a palindrome.", originalInteger);
+    else
+        printf("%d is not a palindrome.", originalInteger);
+    
+   //return 0;
+}
